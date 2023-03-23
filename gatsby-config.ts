67a135863dcb1390b616e3d23a6c8fcaab1f803d
Arttu Mäkinen <ignomagames@gmail.com>
2023-03-23 12:00:10 +0200

    workflow

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/pelipaja",
   siteMetadata: {
     title: `Pelipaja`,
     siteUrl: `https://www.yourdomain.tld`
