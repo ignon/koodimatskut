e2aa59d97ec41ed6381d7e26e25c703cb50f647e
Arttu Mäkinen <ignomagames@gmail.com>
2023-08-04 16:11:19 +0300

    posthog dynamic import

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,17 +1,27 @@
-import posthog from 'posthog-js'
+// dynamic import to not break
+// build-process (where 'window' isn't available)
+const loadPosthog = async () =>
+  (typeof window !== 'undefined')
+    ? (await import('posthog-js')).default
+    : null
 
-posthog.init(
-  'phc_2SLjm4aa2jlGn5vbQ8kJRrKV5D3An9SS8931qxSb23U',
-  {
-    api_host: 'https://app.posthog.com',
-    persistence: 'memory',
-    autocapture: false,
-    capture_pageview: true
-  }
-)
+const init = async () => {
+  const posthog = await loadPosthog()
+  posthog?.init(
+    'phc_2SLjm4aa2jlGn5vbQ8kJRrKV5D3An9SS8931qxSb23U',
+    {
+      api_host: 'https://app.posthog.com',
+      persistence: 'memory',
+      autocapture: false,
+      capture_pageview: true
+    }
+  )
+}
+init()
 
-const sendEvent = (eventId: string, options: object) => {
-  posthog.capture(eventId, options)
+const sendEvent = async (eventId: string, options: object) => {
+  const posthog = await loadPosthog()
+  posthog?.capture(eventId, options)
 
   console.log({
     sendAnalyticsEvent: {
