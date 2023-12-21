8089b57cfb8b7ec05bbfce2bc369ec91c7660b82
Arttu Mäkinen <ignomagames@gmail.com>
2023-12-21 23:16:06 +0200

    filter_test_accounts

--- src/analytics.ts
+++ src/analytics.ts
@@ -5,16 +5,40 @@ const loadPosthog = async () =>
     ? (await import('posthog-js')).default
     : null
 
+
 // This site should never use cookies or local storage
 // but we'll make sure about that in case Posthog
 // or Cusdis start doing something weird
 const clearEverything = () => {
   if (typeof window == 'undefined') return;
-  window.localStorage.clear();
+  clearLocalStorage()
   deleteAllCookies()
 }
 
-function deleteAllCookies() {
+
+const checkForTestAccount = () => {
+  if (typeof window == 'undefined') return null;
+
+  const params = new URLSearchParams(window.location.search)
+  const isTestAccount = params.get('test_account') || localStorage.getItem('test_account')
+  return isTestAccount ? true : false
+
+}
+
+const isTestAccount = checkForTestAccount();
+if (isTestAccount) {
+  localStorage.setItem('test_account', 'true')
+}
+
+const clearLocalStorage = () => {
+  const test_account = window.localStorage.getItem('test_account')
+  window.localStorage.clear();
+  if (test_account) {
+    localStorage.setItem('test_account', 'true')
+  }
+}
+
+const deleteAllCookies = () => {
     if (typeof document == 'undefined') return;
 
     const cookies = document.cookie.split(";");
@@ -29,6 +53,7 @@ function deleteAllCookies() {
 
 const init = async () => {
   clearEverything()
+  checkForTestAccount()
 
   const posthog = await loadPosthog()
   posthog?.init(
@@ -37,14 +62,20 @@ const init = async () => {
       api_host: 'https://eu.posthog.com',
       persistence: 'memory',
       autocapture: false,
-      capture_pageview: true
+      capture_pageview: false
     }
   )
+
+  sendEvent('$pageview')
 }
 init()
 
-const sendEvent = async (eventId: string, options?: object) => {
+const sendEvent = async (eventId: string, options: any = {}) => {
   const posthog = await loadPosthog()
+  if (isTestAccount) {
+    options.test_account = true
+  }
+
   posthog?.capture(eventId, options)
 
   console.log({
